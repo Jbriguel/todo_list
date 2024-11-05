b1c8475542998018fa2c319a0b0be7da546638f0
Abdouljr <maigaabdouljr@gmail.com>
2024-11-05 23:54:41 +0000

    clean code

--- lib/init_view.dart
+++ lib/init_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/main.dart';
 import 'package:todo_list/views/authentification/login_view/login_view.dart';
 
 class InitView extends StatefulWidget {
@@ -13,16 +12,16 @@ class _InitViewState extends State<InitView> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Future.delayed(Duration(seconds: 3)),
+        future: Future.delayed(const Duration(seconds: 3)),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Scaffold(
+            return const Scaffold(
               body: Center(
                 child: CircularProgressIndicator(),
               ),
             );
           } else {
-            return LoginView(); //MyHomePage(title: 'Flutter Demo Home Page');
+            return const LoginView(); //MyHomePage(title: 'Flutter Demo Home Page');
           }
         });
   }
